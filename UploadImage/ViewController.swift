4385fb2c96df953133c5f0e11918b1585de33750
iosdeveloper <iosdeveloper@siliconinfo.com>
2017-09-12 17:40:01 +0530

    //

--- UploadImage/ViewController.swift
+++ UploadImage/ViewController.swift
@@ -11,13 +11,21 @@ import Alamofire
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
+    @IBOutlet var btnSelect: UIButton!
 
+    @IBOutlet var blurView: UIView!
+    @IBOutlet var btnUpload: UIButton!
     @IBOutlet var ActivityIndicator: UIActivityIndicatorView!
     @IBOutlet var img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.ActivityIndicator.isHidden = true
+        self.blurView.isHidden = true
+        
+        self.blurView.layer.cornerRadius = 10;
+        self.blurView.layer.masksToBounds = true;
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -38,21 +46,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func UploadimgClick(_ sender: Any)
     {
+        self.btnSelect.isUserInteractionEnabled = false
+        self.btnUpload.isUserInteractionEnabled = false
         let str = img.image
         self.ActivityIndicator.isHidden = false
         self.ActivityIndicator.startAnimating()
+        self.blurView.isHidden = false
+        
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            self.blurView.backgroundColor = UIColor.clear
+            
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            //always fill the view
+            blurEffectView.frame = self.blurView.bounds
+            blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            self.blurView.addSubview(blurEffectView)
+        }
+        else
+        {
+            self.blurView.backgroundColor = UIColor.black
+        }
         
         let data = UIImageJPEGRepresentation(str!, 1)
         
-        let parameters = ["key" : "Value"]
+        let parameters = ["type" : "image" , "sender" : "" ,"reciver" : ""]
         
         let urlString = "API"
         
         
         Alamofire.upload(multipartFormData: { (multipartFormData) in
-            multipartFormData .append(data!, withName: "Image perameter", fileName: "img.png", mimeType: "image/png")
+            multipartFormData .append(data!, withName: "uploadedfile", fileName: "img.png", mimeType: "image/png")
             
-            for (key, val) in parameters {
+            for (key, val) in parameters
+            {
                 multipartFormData.append(val.data(using: String.Encoding.utf8)!, withName: key)
             }
             
@@ -64,7 +92,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 
                 print(response.result.value!)
                 
-                    if let jsonResponse = response.result.value as? [String: Any]
+                    if let jsonResponse = response.result.value as? [String: Any],let status = jsonResponse["status"] as? Int, status == 1
                     {
                         self.ActivityIndicator.stopAnimating()
                         print(jsonResponse)
@@ -72,21 +100,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                         let alert = UIAlertController(title: "Alert",
                                                       message: "Data Succesfully Saved",
                                                       preferredStyle: UIAlertControllerStyle.alert)
-                        
                         let cancelAction = UIAlertAction(title: "OK",
                                                          style: .cancel, handler: nil)
-                        
                         alert.addAction(cancelAction)
                         self.present(alert, animated: true, completion: nil)
                         
+                        self.btnUpload.isUserInteractionEnabled = true
+                        self.btnSelect.isUserInteractionEnabled = true
                         self.ActivityIndicator.isHidden = true
+                        self.blurView.isHidden = true
+                    }
+                else
+                    {
+                        let alert = UIAlertController(title: "Alert",
+                                                      message: "Profile is not updated",
+                                                      preferredStyle: UIAlertControllerStyle.alert)
+                        let cancelAction = UIAlertAction(title: "OK",
+                                                         style: .cancel, handler: nil)
+                        alert.addAction(cancelAction)
+                        self.present(alert, animated: true, completion: nil)
+
+                        self.btnUpload.isUserInteractionEnabled = true
+                        self.btnSelect.isUserInteractionEnabled = true
+                        self.ActivityIndicator.isHidden = true
+                        self.blurView.isHidden = true
                     }
                 }
             case .failure(let encodingError):
                 print(encodingError)
             }
         }
-
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
